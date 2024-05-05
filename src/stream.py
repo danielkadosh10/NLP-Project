506ea0bcc32ed3cb3166ecf4ac29a051605c8d80
daniel <danielkadosh10@gmail.com>
2024-05-05 03:20:00 -0500

    m

--- src/stream.py
+++ src/stream.py
@@ -3,11 +3,11 @@ from about import about_page
 
 def main():
     st.set_page_config(layout="wide", initial_sidebar_state="expanded",
-                   page_title="Movie Review Sentiment Analysis Scoring", page_icon="🎥")
-    st.title("Movie Review Sentiment Analysis")
+                   page_title="Stock Prediction Using Sentiment Analysis", page_icon="📈")
+    st.title("Stock Prediction Using Sentiment Analysis")
     st.sidebar.title("Navigation")
     selected_tab = st.sidebar.radio(
-        "Go to", ["Movie Scores", "Test the model!", "Model Training Results", "About"], index=0)
+        "Go to", ["About"], index=0)
 
     if selected_tab == "About":
         about_page()
